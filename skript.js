702a3c728b2a3a7d9e2a85ebbae7fd2bcf754b4f
martyasbutkus <noobtuberis1990@gmail.com>
2023-11-16 15:18:50 +0200

    js-1

---
+++ skript.js
@@ -0,0 +1,48 @@
+// PIRMA UZDUOTIS
+var legalAge = 20
+var clientAge
+
+var clientAge = prompt("Iveskite savo amziu")
+
+if(clientAge >= legalAge){
+    alert("Klientas pasiekes " + legalAge)
+} else{
+    var age = confirm(legalAge < clientAge)
+    alert("Palaukite dar: "  +(legalAge - clientAge) + " metu(s).")
+}
+
+
+
+//Antra uzduotis
+var vardas = 'Martynas'
+
+if(vardas.length > 6 ){
+    alert("Vardas per ilgas")
+}
+
+
+
+//Trecia uzduotis
+var myAge = 15
+
+if(myAge <= 0 || myAge >= 101){
+    alert("Invalid age")
+}
+
+if(myAge >= 19){
+    alert("Adult")
+} else if(myAge <= 18){
+    alert("Child")
+
+}
+
+
+var car = "Mini"
+
+if(car == "VW" || car == "Bentley" || car == "Buggati" || car == "Lamborghini" || car == "Proshe" || car == "Audi"){
+    console.log(car + " Priklauso VW group")
+}else if( car == "BMW" || car == "Mini" || car == "Rolls-Royce"){
+    console.log(car + " Priklauso BMW group.")
+}else {
+    console.log(car + " Nepriklausio nei vienam is prekes zenklu.")
+}
